372ddf65067df65cb1c9724af75a5cfd87692535
Johan Andersson von Geijer <johan.andersson.von.geijer@ub.gu.se>
2017-06-21 18:43:43 +0200

    Added deploy config file for staging.

---
+++ config/deploy/staging.rb
@@ -0,0 +1,14 @@
+# Capistrona 3.x Koha Vagrant/Lab stage
+# config/deploy/staging.rb
+
+server 'koha-staging.ub.gu.se',
+  roles: %w{web app db},
+  primary: false,
+  user: 'koha',
+  port: 22,
+  koha_instance_name: 'koha',
+  koha_plack_enabled: false
+
+set :deploy_to, '/home/koha/koha-staging'
+set :branch, 'release-staging-20170621'
+set :keep_releases, 5 # Save space on virtual machines
