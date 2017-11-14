95119c936a39f9a30a90eb5e8192078763e30eb9
David Gustafsson <david.gustafsson@ub.gu.se>
2017-11-14 10:51:14 +0100

    Set new release branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,5 +10,4 @@ server 'koha-staging.ub.gu.se',
   koha_plack_enabled: false
 
 set :deploy_to, '/home/koha/koha-staging'
-set :branch, 'release-20171030'
 set :keep_releases, 5 # Save space on virtual machines
