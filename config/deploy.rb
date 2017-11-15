7315e7500731d8b79f789f7e973a9a5b485fb90e
David Gustafsson <david.gustafsson@ub.gu.se>
2017-11-15 14:33:31 +0100

    Reorder rebase branches alphabetically

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,12 +29,12 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-koha-svc',
   'gub-dev-logo',
   'gub-dev-opac-personal-details-adjustements',
+  'gub-dev-opac-redirect',
   'gub-dev-opac-simplified-messaging',
   'gub-dev-remove-cancel-button',
   'gub-dev-withdrawn-status-details',
   'gub-overdue-messaging',
   'gub-plugin-extender',
-  'gub-dev-opac-redirect',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
