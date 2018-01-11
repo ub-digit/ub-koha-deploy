74ec2d53158a6ec36a135a8c12b924b745da3b09
larsberglen <lars.berglen@ub.gu.se>
2018-01-11 14:55:32 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-acquisitions-fixes',
   'gub-dev-local-translation-files',
   'bug_19893',
+  'gub-dev-syspref-autoapprove-user-profile',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
