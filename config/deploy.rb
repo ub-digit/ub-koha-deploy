bab6c3366cdf6786e93b3d16b4b7ca9262d608a6
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-01-15 17:06:27 +0100

    Added SIP location code branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,6 +45,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-local-translation-files',
   'bug_19893',
   'gub-dev-syspref-autoapprove-user-profile',
+  'gub-dev-sip-send-location-code',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
