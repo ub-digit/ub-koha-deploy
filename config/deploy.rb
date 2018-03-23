3c57a4b750b568a8b7515576fc662eae4c9c3cde
Johan Andersson von Geijer <johan.andersson.von.geijer@ub.gu.se>
2018-03-23 15:50:21 +0100

    Adding release branch release-20180323

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,11 @@
 #lock '3.2.1'
 
 set :application, 'koha'
-set :repo_url, 'https://github.com/ub-digit/Koha.git'
+set :repo_url, 'git@github.com:ub-digit/Koha.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'release-20180313'
+set :branch, 'release-20180323'
 
 set :koha_deploy_release_branch_start_point, 'master'
 set :koha_deploy_rebase_branches, [
