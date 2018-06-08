9819d3979887645e35514eb9e9592f74065451d5
Per Larsson <per.larsson@ub.gu.se>
2018-06-08 16:41:43 +0200

    Add branch gub-dev-fromdate-in-fines

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,6 +82,7 @@ set :koha_deploy_rebase_branches, [
   'gub-bug-20792-fix-patron-edit-page',
   'gub-dev-revert-bug-11512-holdoverride',
   'gub-dev-do-not-backdate-return-via-sip',
+  'gub-dev-fromdate-in-fines',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
