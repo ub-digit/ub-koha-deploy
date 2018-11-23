aea2943092f82959092f4ef55d650183b71425d1
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-11-23 13:53:41 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,6 +92,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-fix-unitprice-decimal',
   'gub-bug-21471-misspelled-items',
   'gub-dev-remove-html-filter',
+  'gub-dev-owning-library-sender',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
