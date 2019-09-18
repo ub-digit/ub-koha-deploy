bdce6cdd33a58e588d0f55aa743ebf20a4da7cd2
Per Larsson <per.larsson@ub.gu.se>
2019-09-18 14:10:00 +0200

    Update release branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_remotes, {
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'release-2019.09-20190918.1338'
+set :branch, 'release-2019.09-20190918.1346'
 
 set :koha_deploy_branches_prefix, ''
 set :koha_deploy_release_branch_prefix, 'release-2019.09-'
