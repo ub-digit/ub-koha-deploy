e9d0c963719a3848b86c58e9e2acc3988d1506c3
Per Larsson <per.larsson@ub.gu.se>
2019-10-23 14:27:33 +0200

    Add branch gub-bug-22539-fines-calculation-fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -110,6 +110,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-syspref-plugin-hook',
   'gub-bug-23655-fix-about-page',
   'gub-bug-23730-fix-export-of-reports',
+  'gub-bug-22539-fines-calculation-fix',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
