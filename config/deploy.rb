0226b21f11f911b2d425c264d0774da9b3e0ec82
David Gustafsson <david.gustafsson@ub.gu.se>
2020-02-14 13:40:29 +0100

    Add branch 'gub-dev-hide-search-sort-options'

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,6 +92,7 @@ set :koha_deploy_rebase_branches, [
   'hides-dateofbirth-and-library-filters-from-patron-search',
   'gub-dev-sort-collation-sv',
   'move-code-from-js-to-tt-template',
+  'gub-dev-hide-search-sort-options',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
