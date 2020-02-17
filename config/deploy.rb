713371cd4cf8c270f11e49f097e564f196e2ff95
David Gustafsson <david.gustafsson@ub.gu.se>
2020-02-17 14:02:25 +0100

    Add branch 'gub-dev-limit-visible-patron-notices'

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,6 +92,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-sort-collation-sv',
   'move-code-from-js-to-tt-template',
   'gub-dev-hide-search-sort-options',
+  'gub-dev-limit-visible-patron-notices',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
