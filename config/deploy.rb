abac9f79c4fcf4858ad22200c9672831db205ae8
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2020-02-27 14:16:27 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,6 +82,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-opac-hide-renew-functionality-when-not-applicable-m',
   'gub-bug-22771-nonfiling-characters-for-sort-fields',
   'gub-dev-acqui-handle-missing-biblio-in-orders',
+  'gub-dev-prevent-accidental-removal-of-library-groups',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
