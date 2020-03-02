ebe7672de5b02c93082c77d00c9ef72b62c415ba
Johan Larsson <johan.larsson@ub.gu.se>
2020-03-02 11:32:34 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -83,6 +83,7 @@ set :koha_deploy_rebase_branches, [
   'gub-bug-22771-nonfiling-characters-for-sort-fields',
   'gub-dev-acqui-handle-missing-biblio-in-orders',
   'gub-dev-prevent-accidental-removal-of-library-groups',
+  'gub-dev-hide-fines-table-if-empty',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
