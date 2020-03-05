7ba2df2b287b9d5ed0dc0247477fa82816656484
David Gustafsson <david.gustafsson@ub.gu.se>
2020-03-05 13:35:31 +0100

    Add new branch 'gub-bug-24807-elasticsearch-sort-empty-values'

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,6 +90,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-hide-editable-date-holds-table',
   'gub-dev-remove-clubs-from-tab-nav',
   'bug-24788-remove-autoloaded-column-accessors',
+  'gub-bug-24807-elasticsearch-sort-empty-values'
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
