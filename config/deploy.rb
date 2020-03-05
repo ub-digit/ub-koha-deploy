25467d13a79cde386cdefe5d7828ea3759e63139
Per Larsson <per.larsson@ub.gu.se>
2020-03-05 15:44:06 +0100

    Set new release branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_remotes, {
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'release-2020.02-20200303.1131'
+set :branch, 'release-2020.02-20200305.1432'
 
 set :koha_deploy_branches_prefix, ''
 set :koha_deploy_release_branch_prefix, 'release-2020.02-'
