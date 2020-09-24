9e647f1ef29f00a7d420465cfdec449bbb9fbdf3
Johan Larsson <johan.larsson@ub.gu.se>
2020-09-24 10:37:21 +0200

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -104,6 +104,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-koha-1528-remove-paid-for-field',
   'gub-dev-koha-1527-alphabetical-sorting-of-accounts',
   'gub-dev-koha-1545-set-permanent-location',
+  'gub-dev-remove-tabs-from-make-payment',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
