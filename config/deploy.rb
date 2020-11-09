80b9688349f1b6a6d976152cec473a8cb0ce7eb9
Per Larsson <per.larsson@ub.gu.se>
2020-11-09 09:51:31 +0100

    Add featurebranch

--- config/deploy.rb
+++ config/deploy.rb
@@ -121,6 +121,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-allow-zero-vendor-id',
   'gub-bug-25758-show-renew-info',
   'gub-dev-fines-run-everyday',
+  'gub-bug-26666-display-address-information-fix',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
