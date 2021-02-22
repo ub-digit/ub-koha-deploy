62f6a03c4ea0ddfd37c54e538d931249a5a1c54a
Johan Larsson <johan.larsson@ub.gu.se>
2021-02-22 13:42:36 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -122,6 +122,7 @@ set :koha_deploy_rebase_branches, [
   'gub-bug-25758-show-renew-info',
   'gub-dev-fines-run-everyday',
   'gub-bug-26666-display-address-information-fix',
+  'gub-bug-koha-1614-email-html-fix'
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
