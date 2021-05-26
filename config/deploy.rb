83b79c7ab16920fbd52cb353af0a74c0fd8bd5a7
David Gustafsson <glasklas@gmail.com>
2021-05-26 17:03:36 +0200

    Add branch 'gub-dev-env-background-images'

--- config/deploy.rb
+++ config/deploy.rb
@@ -126,6 +126,7 @@ set :koha_deploy_rebase_branches, [
   'koha-1662-hide-existing-holds-priority-dropdown',
   'gub-dev-remove-graphics-magick-dep',
   'gub-update-permissions',
+  'gub-dev-env-background-images',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
