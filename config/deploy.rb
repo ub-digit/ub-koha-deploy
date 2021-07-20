401106cb6853bc7f42ef16b89cb1bf23a8e513eb
David Gustafsson <glasklas@gmail.com>
2021-07-20 13:44:37 +0200

    Rename branch 'gub-dev-env-background-images'

--- config/deploy.rb
+++ config/deploy.rb
@@ -126,7 +126,7 @@ set :koha_deploy_rebase_branches, [
   'koha-1662-hide-existing-holds-priority-dropdown',
   'gub-dev-remove-graphics-magick-dep',
   'gub-update-permissions',
-  'gub-dev-env-background-images',
+  'gub-dev-environment-assets',
   'gub-dev-anonymize-db',
   'koha-deploy'
 ]
