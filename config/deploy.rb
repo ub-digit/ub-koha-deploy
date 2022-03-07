cbbe72be30e9546d6a93ce9d25fef0f4af2ff744
Per Larsson <per.larsson@ub.gu.se>
2022-03-07 17:42:38 +0100

    Update branch list

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@
 
 set :application, 'koha'
 #set :repo_url, 'https://github.com/ub-digit/Koha.git'
-set :repo_url, 'git@github.com:ub-digit/Koha.git'
+set :repo_url, 'git@github.com:ub-digit/Koha-build.git'
 
 set :repo_remotes, {
   'koha-build' => 'git@github.com:ub-digit/Koha-build.git'
@@ -16,25 +16,28 @@ set :repo_remotes, {
 set :branch, 'release-2020.09-20210630.1325-hotfix-001'
 
 set :koha_deploy_branches_prefix, ''
-set :koha_deploy_release_branch_prefix, 'release-2021.09-'
+set :koha_deploy_release_branch_prefix, 'release-2022.02-'
 set :koha_deploy_release_branch_start_point, 'koha-build-master'
 
 set :koha_deploy_rebase_branches, [
-  'gub-bug-14957-marc-permissions-tmp',
+#  'gub-bug-14957-marc-permissions-tmp', #med i master
   'gub-bug-18129-staged-imports-user-filter',
   'gub-bug-18138-marc-modification-template-on-biblio-save',
   'gub-bug-19707-elasticsearch-sync-mappings-work',
-  'gub-bug-25539-remove-defer-marc-save',
-  'bulkmarcimport',
+#  'gub-bug-25539-remove-defer-marc-save', #med i gub-bug-29440-25539-29597-29654-bulkmarcimport
+#  'bulkmarcimport', #med i gub-bug-29440-25539-29597-29654-bulkmarcimport
+  'gub-bug-29440-25539-29597-29654-bulkmarcimport',
   'gub-dev-remove-lost-item-refund-msg',
   'gub-dev-koha-svc',
-  'gub-dev-opac-simplified-messaging',
+  'gub-dev-simplified-messaging',
   'gub-overdue-messaging',
   'gub-plugin-extender',
-  'gub-dev-acquisitions-fixes',
+#  'gub-dev-acquisitions-fixes', #delas upp i 2 följande
+  'gub-dev-acquisitions-uncertain-price-on-importing',
+  'gub-dev-acquisitions-recalculate-price',
   'gub-bug-20058-sip-send-location-code',
-  'gub-dev-favicon-for-my-loans',
-  'gub-dev-css-for-slip-prints',
+#  'gub-dev-favicon-for-my-loans', #obsolet
+#  'gub-dev-css-for-slip-prints', #flyttas till statisk fil
   'gub-dev-set-focus-on-confirm-hold-and-transfer-button',
   'gub-dev-advanced-search-customizations',
   'gub-bug-29145-overdue-debarments-fix',
@@ -48,18 +51,18 @@ set :koha_deploy_rebase_branches, [
   'gub-bug-23009-deleted-marc-conditions',
   'gub-dev-cache-subscription-frequencies',
   'gub-change-sort-order-and-paging-for-table-subscription-numberpatterns',
-  'gub-dev-opac-minalan',
+#  'gub-dev-opac-minalan', #obsolet
   'gub-dev-show-852-in-biblio',
-  'gub-dev-do-not-backdate-return-via-sip',
-  'gub-dev-fromdate-in-fines',
+#  'gub-dev-do-not-backdate-return-via-sip',
+#  'gub-dev-fromdate-in-fines',
   'gub-bug-20262-refund-lost-only-if-unpaid',
   'gub-dev-fix-unitprice-decimal',
   'gub-dev-incomplete-barcode',
   'gub-bug-23548-aq-field-required',
-  'gub-dev-reset-expiration-on-revert',
+#  'gub-dev-reset-expiration-on-revert',
   'gub-dev-plugin-hooks',
   'gub-dev-plugin-hooks-update-status',
-  'hides-dateofbirth-and-library-filters-from-patron-search',
+  'gub-dev-hides-dateofbirth-and-library-filters-from-patron-search',
   'gub-dev-sort-collation-sv',
   'gub-dev-hide-checkout-history-button-in-biblio-view',
   'gub-dev-hides-circulation-history-and-holds-history-in-patron-post',
@@ -73,7 +76,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-acqusition-form-hide-unused-items',
   'gub-dev-mandatory-account-selection-in-acqusition',
   'gub-bug-24720-special-chars-normalize-sort-fields',
-  'gub-dev-hide-fines-table-if-empty',
+#  'gub-dev-hide-fines-table-if-empty', #obsolet
   'auto-renew-borrower-account-cron',
   'gub-dev-intra-hide-revert-waiting-btn',
   'gub-dev-hide-editable-date-holds-table',
@@ -84,20 +87,20 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-koha-1527-alphabetical-sorting-of-accounts',
   'gub-dev-koha-1545-set-permanent-location',
   'gub-dev-remove-tabs-from-make-payment',
-  'gub-dev-koha-1568-broken-filter-in-transactions',
-  'gub-bug-15775-maxoutstanding-alert',
+#  'gub-dev-koha-1568-broken-filter-in-transactions', #verkar vara löst i master
+#  'gub-bug-15775-maxoutstanding-alert', #obsolet
   'gub-bug-27859-marc-export-search-result',
   'gub-dev-set-default-biblio-in-orders-when-missing',
   'gub-dev-lost-status-update-for-paid-issues',
   'gub-dev-force-default-replacementcost',
   'gub-dev-allow-zero-vendor-id',
-  'gub-bug-koha-1614-email-html-fix',
+#  'gub-bug-koha-1614-email-html-fix', #löst i master
   'koha-1662-hide-existing-holds-priority-dropdown',
   'gub-dev-remove-graphics-magick-dep',
   'gub-update-permissions',
   'gub-dev-environment-assets',
-  'gub-dev-disable-regexp-replace-in-migration',
-  'gub-dev-disable-analytics-link',
+#  'gub-dev-disable-regexp-replace-in-migration',
+#  'gub-dev-disable-analytics-link', #kolla om detta är löst
   'gub-dev-cpanfile-version-specification',
   'koha-deploy'
 ]
