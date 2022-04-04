5939af081a3cee48ec7012970a2a6f163ce32559
Per Larsson <per.larsson@ub.gu.se>
2022-04-04 13:46:14 +0200

    Add branch gub-dev-sip-password-from-attribute

--- config/deploy.rb
+++ config/deploy.rb
@@ -104,6 +104,7 @@ set :koha_deploy_rebase_branches, [
   'gub-dev-cpanfile-version-specification',
   'bug-30255-batchmod-optional-list-step',
   'gub-dev-anonymize-db',
+  'gub-dev-sip-password-from-attribute',
   'koha-deploy'
 ]
 #set :koha_deploy_merge_branches, [
