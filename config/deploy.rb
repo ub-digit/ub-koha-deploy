07478413166bd044a77233a8b5819544bd0dac6f
Per Larsson <per.larsson@ub.gu.se>
2025-10-08 11:37:02 +0200

    Set new releasebranch

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :repo_remotes, {
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'release-2025.01-20250915.1226'
+set :branch, 'release-2025.01-20251008.1117'
 
 set :koha_deploy_branches_prefix, ''
 set :koha_deploy_release_branch_prefix, 'release-2025.01-'
