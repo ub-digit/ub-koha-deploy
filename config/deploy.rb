0f6143a46312077356e29a6a77c07abae0bb0362
Per Larsson <per.larsson@ub.gu.se>
2026-02-06 10:09:21 +0100

    Set new release branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :repo_remotes, {
 }
 
 # Default branch is :master
-set :branch, 'release-2025.01-20251125.1441'
+set :branch, 'release-2026.02-20260205.2226'
 
 set :koha_deploy_branches_prefix, ''
 set :koha_deploy_release_branch_prefix, 'release-2026.02-'
